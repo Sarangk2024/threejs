3341662bcff8d0119cb11eb527f50afcf4368128
Sarang K <kuttappisarang629@gmail.com>
2026-02-23 00:56:13 +0530

    chnaged server.js

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,6 @@ app.post('/send-sms', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => 
-    console.log(`Portfolio backend running on port ${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Portfolio backend running on port ${PORT}`);
+});
